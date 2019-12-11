2dfb1a97d80051abaf25a2434cb3846a2fa92fed
Shahar <shachar.fo@gmail.com>
2019-12-12 00:05:51 +0200

    updates

--- Ex1/Graph.java
+++ Ex1/Graph.java
@@ -1,24 +1,17 @@
 package Ex1;
 
 public class Graph {
-	public int Width;
-	public int Height;
-	public int resolution;
-	public double[] rx = new double[2];
-	public double[] ry=  new double[2];
+	public static int Width;
+	public static int Height;
+	public static int Resolution;
+	public static double[]Range_X = new double[2];
+	public static double[] Range_Y=  new double[2];
 	/**
 	 * @param width
 	 * @param height
 	 * @param resolution
-	 * @param range_X
-	 * @param range_Y
-	 */
-	public Graph(int width, int height, int resolution, double[] rx, double[] ry) {
-		this.Width = width;
-		this.Height = height;
-		this.resolution = resolution;
-		this.rx = rx;
-		this.ry = ry;
-	}
+	 * @param rx
+	 * @param ry
+*/
 
 }
